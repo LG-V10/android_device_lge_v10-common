5f71ddd5c84b7aa6d065376ca0229639bd0e108d
Eliminater74 <eliminater74@gmail.com>
2018-06-11 19:46:48 -0400

    [v10-common] Fix Camera Android.mk Error:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- camera/Android.mk
+++ camera/Android.mk
@@ -20,7 +20,7 @@ include $(CLEAR_VARS)
 ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
   ifneq ($(strip $(USE_CAMERA_STUB)),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
-      include $(call all-subdir-makefiles)
+      include $(call all-makefiles-under,$(call $(LOCAL_PATH)))
     endif
   endif
 endif
